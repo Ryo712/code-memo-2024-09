71083b9442215b15b6b2541e7fb36f9c6dbeae55
大城亮 <hiltoc.katsu@gmail.com>
2024-10-01 22:21:47 +0900

    UIとロジックのファイル

--- my-next-app/lib/utils.ts
+++ my-next-app/lib/utils.ts
@@ -1,14 +1,12 @@
 // lib/utils.ts
-export function calculateSum(a: number, b: number): number {
+
+// 2つの数値を足し合わせる関数
+export function sum(a: number, b: number): number {
     return a + b;
   }
   
-  export function calculateDifference(a: number, b: number): number {
-    return a - b;
+  // 2つの数値を掛け合わせる関数
+  export function multiply(a: number, b: number): number {
+    return a * b;
   }
-/*このファイルにはReactの要素は含まれず、純粋なTypeScriptのロジックを書きます。単に数値を足し算や引き算する関数が定義されています。
-この関数をReactコンポーネント内で使うことができます。数値を計算するための関数が含まれています。
-JavaScriptのロジックや関数をTypeScriptで記述しています。ここでは、数値を計算するための関数が含まれています。*/
-
-
   
